a2e5751f70c5abdc4bd91ad763435a96493e5546
IslamMuha <islammuhamatulin736@gmail.com>
2025-06-01 05:04:25 +0500

    Flask запускается на 0.0.0.0 для Docker

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 from app import create_app
+import os
 
 # Создаём экземпляр приложения через фабрику
 app = create_app()
 
 # Запускаем сервер в режиме отладки
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run( 
+        host=os.getenv("FLASK_RUN_HOST", "127.0.0.1"),
+        debug=os.getenv("FLASK_ENV") == "development"
+        )
